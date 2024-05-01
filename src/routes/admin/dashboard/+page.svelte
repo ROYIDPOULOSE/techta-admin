8a53b8bd5e6d2bcc1bb56a03cb0277748b181626
royid_poulose <royidcp96@gmail.com>
2024-05-01 18:06:41 +0530

    dashboard page

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import type { PageData } from './$types';
-    
     export let data: PageData;
 </script>
+
+<!-- <div class="flex items-center justify-center h-screen">
+    <div>
+        <h2>Dashboard</h2>
+    </div>
+</div> -->
