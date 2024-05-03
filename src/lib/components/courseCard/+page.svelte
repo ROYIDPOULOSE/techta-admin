974a4e21deb1001112e6cd715166a45c9cb7d66c
royid_poulose <royidcp96@gmail.com>
2024-05-03 20:18:14 +0530

    New component course card created

---
+++ src/lib/components/courseCard/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root>
+    <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+        <Card.Title class="text-sm font-bold">2D Drafting and Plotting</Card.Title>
+    </Card.Header>
+    <Card.Content>
+        <p class="text-xs text-muted-foreground">Auto CAD</p>
+        <p class="text-xs text-muted-foreground">Microstation</p>
+    </Card.Content>
+</Card.Root>
