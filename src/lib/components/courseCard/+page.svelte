5455c7239a840553f7c9de316687308a84ad4675
royid_poulose <royidcp96@gmail.com>
2024-05-04 13:17:29 +0530

    fetching data to the card

--- src/lib/components/courseCard/+page.svelte
+++ src/lib/components/courseCard/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+
+    export let course: { course: string; software: string; duration: number };
 </script>
 
 <Card.Root>
     <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-        <Card.Title class="text-sm font-bold">2D Drafting and Plotting</Card.Title>
+        <Card.Title class="text-sm font-bold">{course.course}</Card.Title>
     </Card.Header>
     <Card.Content>
-        <p class="text-xs text-muted-foreground">Auto CAD</p>
-        <p class="text-xs text-muted-foreground">Microstation</p>
+        <p class="text-xs text-muted-foreground">{course.software}</p>
+        <p class="text-xs text-muted-foreground">{course.duration} hours</p>
     </Card.Content>
 </Card.Root>
