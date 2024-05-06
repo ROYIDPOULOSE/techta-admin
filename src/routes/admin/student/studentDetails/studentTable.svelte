4225c727de760fbafb54f1187738292f4d0af8bc
royid_poulose <royidcp96@gmail.com>
2024-05-06 17:08:20 +0530

    Updated student table to view student data

--- src/routes/admin/student/studentDetails/studentTable.svelte
+++ src/routes/admin/student/studentDetails/studentTable.svelte
@@ -1,76 +1,20 @@
 <script lang="ts">
     import * as Table from "$lib/components/ui/table/index.js";
+    import { createEventDispatcher } from 'svelte';
+    import { Button } from '$lib/components/ui/button';
+    import type { StudentData } from './types';
    
-    const student = [
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-      {
-        studentID: "INV001",
-        name: "Paid",
-        address: "$250.00",
-        phone: "Credit Card",
-        status: "Active",
-        totalAmount:"$250.00",
-      },
-    ];
+    export let students: StudentData[];
+
+    const dispatch = createEventDispatcher();
+
+    function handleEditClick(student: StudentData) {
+        dispatch('edit', student);
+    }
   </script>
    
   <Table.Root>
-    <Table.Caption>Student Details.</Table.Caption>
+    <Table.Caption>Student Details</Table.Caption>
     <Table.Header>
       <Table.Row>
         <Table.Head class="w-[100px]">Student ID</Table.Head>
@@ -78,18 +22,24 @@
         <Table.Head>Address</Table.Head>
         <Table.Head>Phone</Table.Head>
         <Table.Head>Status</Table.Head>
-        <Table.Head class="text-right">Amount</Table.Head>
+        <Table.Head>Course</Table.Head>
+        <Table.Head class="text-right">Actions</Table.Head>
       </Table.Row>
     </Table.Header>
     <Table.Body>
-      {#each student as student, i (i)}
+      {#each students as student, i (student.id)}
         <Table.Row>
           <Table.Cell class="font-medium">{student.studentID}</Table.Cell>
           <Table.Cell>{student.name}</Table.Cell>
           <Table.Cell>{student.address}</Table.Cell>
           <Table.Cell>{student.phone}</Table.Cell>
           <Table.Cell>{student.status}</Table.Cell>
-          <Table.Cell class="text-right">{student.totalAmount}</Table.Cell>
+          <Table.Cell>{student.course}</Table.Cell>
+          <Table.Cell class="text-right">
+            <Button 
+            class="font-medium text-blue-600 dark:text-blue-500 hover:underline" 
+            on:click={() => handleEditClick(student)}>Edit</Button>
+          </Table.Cell>
         </Table.Row>
       {/each}
     </Table.Body>
