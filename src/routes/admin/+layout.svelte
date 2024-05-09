3a029a7b1a6c3e0173d0d3ab578bc826c5fe7608
royid_poulose <royidcp96@gmail.com>
2024-05-09 15:09:10 +0530

    code cleaning

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
     import Header from '$lib/components/Header.svelte';
-    
-    export let data: LayoutData;
 </script>
 
 <div class="relative min-h-screen w-full bg-muted  dark:bg-neutral-950 text-foreground">
