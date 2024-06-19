0f07a07f644b911f0f7a68b7aa8707e5645b34a5
Royid Poulose <royidcp96@gmail.com>
2024-06-19 11:15:02 +0530

    change in firebase initailization

--- src/lib/services/firebase.ts
+++ src/lib/services/firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,27 +20,38 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 // Get a reference to the Firestore database
-const db = getFirestore(app);
+let firebaseApp;
+if (!getApps().length){
+  firebaseApp = initializeApp(firebaseConfig)
+} else {
+  firebaseApp =getApp()
+  deleteApp(firebaseApp)
+  firebaseApp = (firebaseApp)
+}
 
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 // Initialize Cloud Storage and get a reference to the service
-export const storage = getStorage(app);
+export const storage = getStorage(firebaseApp);
+
 
 // Check if the current environment supports Firebase Analytics
-isSupported()
-  .then((supported) => {
-    if (supported) {
-      // Initialize Firebase Analytics only if it's supported (browser environment)
-      const analytics = getAnalytics(app);
-      console.log("Firebase Analytics initialized");
-    } else {
-      console.log("Firebase Analytics not supported in this environment");
-    }
-  })
-  .catch((error) => {
-    console.error("Error checking Firebase Analytics support:", error);
-  });
+// isSupported()
+//   .then((supported) => {
+//     if (supported) {
+//       // Initialize Firebase Analytics only if it's supported (browser environment)
+//       const analytics = getAnalytics(app);
+//       console.log("Firebase Analytics initialized");
+//     } else {
+//       console.log("Firebase Analytics not supported in this environment");
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error checking Firebase Analytics support:", error);
+//   });
 
-export { app, db };
+// export { app, db };
+export { db, auth };
