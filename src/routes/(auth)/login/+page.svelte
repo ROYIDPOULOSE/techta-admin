1f8351cf40aa690286e348ec79fb64da6c8548a6
Royid Poulose <royidcp96@gmail.com>
2024-06-19 11:19:56 +0530

    User authentication

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -4,15 +4,41 @@
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
     import { goto } from '$app/navigation';
+    import { Loader } from 'lucide-svelte';
+    import { authHandlers } from '$lib/store/store';
 
     let email: string = '';
     let password: string = '';
+    let error = false;
+    let authenticating = false;
+    let register = false;
 
     function handleLogin() {
         if (email === 'admin' && password === 'admin') {
             goto('/admin/dashboard');
         } else {
             alert('Invalid email or password');
+        }
+    }
+
+    async function handleAuthenticate() {
+        if (authenticating) {
+            return;
+        }
+        if (!email || !password) {
+            error = true;
+            return;
+        }
+        authenticating = true;
+        
+        try {
+            if(!register){
+                await authHandlers.login(email, password);
+            }
+        } catch (err){
+            console.log("There was an auth error", err);
+            error = true;
+            authenticating = false;
         }
     }
 </script>
@@ -24,6 +50,9 @@
             <Card.Description>Enter your email below to login to your account.</Card.Description>
         </Card.Header>
         <Card.Content class="grid gap-4">
+            {#if error}
+                <p class="text-red-700 text-sm"> The information you have entered is not correct</p>
+            {/if}
             <div class="grid gap-2">
                 <Label for="email">Email</Label>
                 <Input id="email" type="email" placeholder="m@example.com" required bind:value={email} />
@@ -34,7 +63,13 @@
             </div>
         </Card.Content>
         <Card.Footer>
-            <Button class="w-full" on:click={handleLogin}>Sign in</Button>
+            <Button class="w-full" on:click={handleAuthenticate}>
+                {#if authenticating}
+                    <Loader class="animate-spin"/>
+                {:else}
+                    Sign in
+                {/if}
+            </Button>
         </Card.Footer>
     </Card.Root>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,58 @@
 <script>
     import "../app.css";
+    import { onMount } from "svelte";
+    import { auth, db } from '$lib/services/firebase'
+    import { getDoc, doc, setDoc } from 'firebase/firestore'
+    import { authStore } from '$lib/store/store'
+
+    const nonAuthRoutes = ['/']
+    onMount(() => {
+        console.log('Mounting');
+        const unsubscribe = auth.onAuthStateChanged(async user => {
+            const currentPath = window.location.pathname;
+
+            if (!user && !nonAuthRoutes.includes(currentPath)) {
+                window.location.href = "/";
+                return;
+            }
+
+            if (user && currentPath === "/login") {
+                window.location.href = "/admin/dashboard";
+                return;
+            }
+
+            if (!user) {
+                return;
+            }
+
+            let dataToSetToStore;
+            const docRef = doc(db, 'users', user.uid);
+            const docSnap = await getDoc(docRef);
+            if (!docSnap.exists()) {
+                const userRef = doc(db, "user", user.uid);
+                dataToSetToStore = {
+                    email: user.email,
+                    todos: [],
+                }
+                await setDoc(
+                    userRef, dataToSetToStore, { merge: true});
+            } else {
+                const userData = docSnap.data();
+                dataToSetToStore = userData;
+            }
+
+            authStore.update((curr) => {
+                return {
+                    ...curr,
+                    user,
+                    data: dataToSetToStore,
+                    loading: false,
+                };
+            });
+        });
+    });
 </script>
 
-<slot/>
+<div>
+    <slot/>
+</div>
